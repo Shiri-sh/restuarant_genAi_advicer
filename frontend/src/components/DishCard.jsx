0e4c0641ad72a9c45057e572d86bcc1982e68dd3
mimi-brandes <ms0583242931@gmail.com>
2025-12-15 17:31:27 +0200

    icons_picture

--- frontend/src/components/DishCard.jsx
+++ frontend/src/components/DishCard.jsx
@@ -1,3 +1,4 @@
+import { Salad, Vegan,Bot} from 'lucide-react';
 export default function DishCard({ dish }) {
   const veganBadge = "/images/vegan-badge.png";
   const vegetarianBadge = "/images/vegetarian-badge.jpg";
@@ -11,19 +12,23 @@ export default function DishCard({ dish }) {
       {dish.on_sale ? (
         <p className="dish-sale-price">Sale Price: ${dish.sale_price}</p>
       ) : null}
-      {dish.is_vegan && (
-        <div
-          className="diet-badge"
-          style={{ backgroundImage: `url(${veganBadge})` }}
-        />
-      )}
-      {dish.is_vegetarian && !dish.is_vegan && (
-        <div
-          className="diet-badge"
-          style={{ backgroundImage: `url(${vegetarianBadge})` }}
-        />
-      )}
-      <p className="dish-reason">{dish.reason}</p>
+     
+
+      <p className="dish-description">{dish.description}</p>
+      <p className="dish-reason"><Bot className="bot"/><span>{dish.reason}</span></p>
+      {/* <div className="diet-row">
+  {dish.is_vegan && <Vegan className="diet-icon" size={28} strokeWidth={2}/>}
+  {dish.is_vegetarian && !dish.is_vegan && <Salad className="diet-icon" size={28} strokeWidth={2}/>}
+</div> */}
+<div className="diet-row">
+        {dish.is_vegan
+          ? <Vegan className="diet-icon" size={28} strokeWidth={2} />
+          : null}
+        {dish.is_vegetarian && !dish.is_vegan
+          ? <Salad className="diet-icon" size={28} strokeWidth={2} />
+          : null}
+      </div>
     </div>
   );
 }
+
