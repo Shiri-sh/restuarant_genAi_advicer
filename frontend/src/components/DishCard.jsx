75004968e1f01e2a2a0dc906e52d6ec4795f0eb3
mimi-brandes <ms0583242931@gmail.com>
2025-12-16 14:11:40 +0200

    style

--- frontend/src/components/DishCard.jsx
+++ frontend/src/components/DishCard.jsx
@@ -1,4 +1,4 @@
-import { Salad, Vegan,Bot} from 'lucide-react';
+import { Salad, Vegan, Bot } from 'lucide-react';
 export default function DishCard({ dish }) {
   const veganBadge = "/images/vegan-badge.png";
   const vegetarianBadge = "/images/vegetarian-badge.jpg";
@@ -7,27 +7,25 @@ export default function DishCard({ dish }) {
     <div className="dish-card">
       <img src={`http://localhost:5000${dish.image_url}`} alt={dish.name} className="dish-image" />
       <h2 className="dish-name">{dish.name}</h2>
-      <p className="dish-price">Price: ${dish.price}</p>
-      <p className="dish-ingredients">Ingredients: {dish.ingredients}</p>
+      <p className="dish-price"><span className="bold">Price:</span>   {dish.price}₪</p>
+      <p className="dish-ingredients"><span className="bold">Ingredients:</span>   {dish.ingredients}</p>
       {dish.on_sale ? (
-        <p className="dish-sale-price">Sale Price: ${dish.sale_price}</p>
+        <p className="dish-sale-price"><span className="bold">Sale Price:</span> ${dish.sale_price}</p>
       ) : null}
-     
-
       <p className="dish-description">{dish.description}</p>
-      <p className="dish-reason"><Bot className="bot"/><span>{dish.reason}</span></p>
-      {/* <div className="diet-row">
-  {dish.is_vegan && <Vegan className="diet-icon" size={28} strokeWidth={2}/>}
-  {dish.is_vegetarian && !dish.is_vegan && <Salad className="diet-icon" size={28} strokeWidth={2}/>}
-</div> */}
-<div className="diet-row">
-        {dish.is_vegan
-          ? <Vegan className="diet-icon" size={28} strokeWidth={2} />
-          : null}
-        {dish.is_vegetarian && !dish.is_vegan
-          ? <Salad className="diet-icon" size={28} strokeWidth={2} />
-          : null}
-      </div>
+      <p>-----------------------------------------------</p>
+      <div className='bot-row'>
+      <Bot className="bot" />
+       </div>
+       <p className="dish-reason">{dish.reason}</p>
+      <div className="diet-row">
+        {dish.is_vegan
+          ? <Vegan className="diet-icon" size={28} strokeWidth={2} />
+          : null}
+        {dish.is_vegetarian && !dish.is_vegan
+          ? <Salad className="diet-icon" size={28} strokeWidth={2} />
+          : null}
+      </div>
     </div>
   );
 }
