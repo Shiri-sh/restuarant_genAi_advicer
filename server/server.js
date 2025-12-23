0603bb94b4261c7651ed119de1cdec8cfe25fbd4
shiri shahor <shirishahor@gmail.com>
2025-12-23 15:50:53 +0200

    port 5000

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/api", audioAnalyzeRouter);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Database Host: ${process.env.MYSQL_HOST}`);
