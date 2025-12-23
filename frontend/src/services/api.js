c0467f4142693a2085a7aade626a8c4047fdb74d
mimi-brandes <ms0583242931@gmail.com>
2025-12-23 14:16:54 +0200

    change ports to 5001 and 3001 to avoid Jenkins conflict

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ export const sendAudioToServer = async (audioBlob) => {
     const formData = new FormData();
     formData.append("audio", audioBlob); 
   
-    const res = await fetch("http://localhost:5000/api/analyze-audio", {
+    const res = await fetch("http://localhost:5001/api/analyze-audio", {
       method: "POST",
       body: formData,
     });
